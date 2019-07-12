9f70e59d126611c64804f8a5a6084023fa1d8d2f
SY <47776658+sung7760@users.noreply.github.com>
2019-07-13 00:56:29 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -565,7 +565,7 @@ async def on_ready():
 
 	# 디스코드에는 현재 본인이 어떤 게임을 플레이하는지 보여주는 기능이 있습니다.
 	# 이 기능을 사용하여 봇의 상태를 간단하게 출력해줄 수 있습니다.
-	await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="여어!히사시부리!", type=1))
+	await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="보스타임봇입니다!", type=1))
 
 	
 # 봇이 새로운 메시지를 수신했을때 동작되는 코드입니다.
